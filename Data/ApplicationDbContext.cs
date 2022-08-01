0225ecf3c55d8c9bd904a40f5925a7c00b21f38b
NiKKuDr <nikolai_kudriashov@yahoo.com>
2022-08-01 03:08:46 -0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Dapper.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dapper.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dapper.Models
+{
+    public class Company
+    {
+        [Key]  // optional refinement since CompanyId is the Primary key by default due to its Id ending
+        public int CompanyId { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
